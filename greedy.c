cac8628586d436ffafda2d4abd745a848f3144d7
dataeconomist <ravindrapimento@gmail.com>
2015-08-02 12:59:55 -0500

    Create greedy.c

---
+++ greedy.c
@@ -0,0 +1,57 @@
+﻿#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+ 
+int main (void)
+{
+        float change;
+        float quarter = .25;
+        float dime = .1;
+        float nickel  = .05;
+        float penny =  .01;
+        int dimes;
+        int quarters
+        int nickels;
+        int
+ 
+        {
+                printf("Yo how much change you need? ");
+                float change = GetFloat();
+        }
+   
+    do
+        {
+            change % quarter
+                 
+        )
+    while change > .25
+         
+        do
+        {
+            change % quarter
+                 
+        )
+    while change > .25
+        do
+        {
+            change % quarter
+                 
+        )
+    while change > .25
+         
+        do
+        {
+            change % quarter
+                 
+        )
+    while change > .25
+         
+        do
+        {
+            change % quarter
+                 
+        )
+    while change > .25
+         
+    printf("well you will have to pay out %d coins", coins)
+}
